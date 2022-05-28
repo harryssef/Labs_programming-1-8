12485b0aaf3e52ad9137de0a4786cee29e424f3c
harryssef <89862791+harryssef@users.noreply.github.com>
2022-05-28 15:29:46 +0300

    Add files via upload

---
+++ 1_1.h
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void change_pointer(int* pointer1, int* pointer2);
+
+void change_link(int& link1, int& link2);
+
+
+
+void change_back_pointer(float* pointer);
+
+void change_back_link(float& link);
+
+
+
+void reduce_pointer(int* pointer1, int* pointer2);
+
+void reduce_link(int& link1, int& link2);
+
+
+
+void swap_pointer(int* pointer_for_line, int* pointer_for_column, int* pointer_for_string1, int* pointer_for_string2);
+
+
+void swap_link(int &link_for_line, int& link_for_column, int& link_for_string1, int& link_for_string2);
